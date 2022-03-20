e6112da079600aa7804c69e523b93b3427008aa4
aleynayasar <79570388+aleynayasar@users.noreply.github.com>
2022-03-20 18:52:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassDomainAccountEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rcn = new Rectangle();
+            Console.Write("Please enter most : ");
+            rcn.Most = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter size ");
+            rcn.Size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nSize: "+rcn.Size + "\nMost: " + rcn.Most+ "\nDomain: " + rcn.Domain);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDomainAccountEncapsulation
+{
+    class Rectangle
+    {
+        private int size;
+        public int Size
+        {
+            get { return size; }
+            set
+            {
+                if (value<0)
+                {
+                    size = 1;
+                }
+                else
+                size = value; 
+            }
+        }
+        private int most;
+        public int Most
+        {
+            get { return most; }
+            set {
+                if (value < 0)
+                { most = 1; }
+                else
+                { most = value; }
+            }
+        }
+        private int domain;
+        public int Domain
+        {
+           get { return domain = size * most; } set { domain = value; }
+        }
+    }
+}
